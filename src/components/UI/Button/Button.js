e7585415d1306f0947218913d7abb6d9ba57cdb7
Daniel <peer77@gmail.com>
2018-07-10 15:28:29 +0300

    lec 128 - addding button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Button.css'
+
+const button = (props) => (
+    <button className={[]}>
+        {props.childrem}
+    </button>
+);
+
+export default button;
