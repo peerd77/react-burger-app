cd731c9745c35c47e492657237d0533b06236d2c
Daniel <peer77@gmail.com>
2018-07-15 14:56:16 +0300

    lec 211 - retrieving ingredients from query string

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -30,6 +30,15 @@ class Checkout extends Component {
             </div>
         );
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients : ingredients});
+    }
 }
 
 export default Checkout;
